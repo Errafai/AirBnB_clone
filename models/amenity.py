6f3f1d29d6196ba183459e1d841ed50bef321866
Errafai <errafaiachraf1923@gmail.com>
2024-08-31 23:59:39 +0100

    adding some classes

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""reprsent the Amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    name = ""
+
---
+++ models/state.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""represent the state class"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    name = ""
